887198a4d8ad545613a176e729da2c4b67de0372
bullitbd <bobd@rialtoadvisors.com>
2015-04-28 18:31:59 -0700

    Homework Commit

---
+++ test/hello_test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var expect = require('chai').expect;
+var hello = require('../hello');
+
+
+describe('hello', function() {
+  it('should greet someone', function() {
+    expect(hello.greet('captain')).to.equal('hello captain');
+  })
+});
+
